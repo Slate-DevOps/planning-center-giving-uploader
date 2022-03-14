7c26fc008407741d1506512677cadb6e9eefaa2f
Liam Ballantyne <ljjb97@gmail.com>
2022-03-14 09:08:04 -0400

    fixed address

--- src/deploy/main.tsx
+++ src/deploy/main.tsx
@@ -26,7 +26,7 @@ const options = {
   scopes: 'people+giving', // Scopes limit access for OAuth tokens.
 };
 
-const url = 'https://pco-giving-loader.deno.dev/';
+const url = 'https://pco-giving-loader.deno.dev';
 const port = 4444;
 const api_url = 'https://api.planningcenteronline.com/oauth/authorize'
 
