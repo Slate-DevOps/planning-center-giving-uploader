95d6de907277123d8c743252a82d6be59ca20de3
Micah Stairs <micah.stairs@gmail.com>
2023-04-19 22:41:31 -0400

    Attempt #3 for the submit disabling

--- src/deploy/main.tsx
+++ src/deploy/main.tsx
@@ -146,7 +146,7 @@ router.get('/ws', async ctx => {
     document.getElementById("selected_file_name").innerHTML = data.get("file").name;
   });
   form.addEventListener('submit', async function(e) {
-    $("#submit").attr("disabled", true);
+    document.getElementById("submit").disabled = true;
     e.preventDefault();
     let fileRead = new FileReader();
     const data = new FormData(form);
